c46ea443d74f5de2360d300899683ac6795a01d0
Intisy <wilderpizza99@gmail.com>
2026-01-27 17:52:36 +0100

    Refactor dependency handling for multi-project support

--- src/main/java/io/github/intisy/gradle/github/utils/GradleUtils.java
+++ src/main/java/io/github/intisy/gradle/github/utils/GradleUtils.java
@@ -1,7 +1,11 @@
 package io.github.intisy.gradle.github.utils;
 
+import org.gradle.api.Project;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * This utility class provides methods for interacting with Gradle.
@@ -17,5 +21,31 @@ public class GradleUtils {
     public static Path getGradleHome() {
         String userHome = System.getProperty("user.home");
         return Paths.get(userHome, ".gradle", "caches");
+    }
+
+    /**
+     * Retrieves all projects recursively, starting from the specified project and including
+     * its subprojects, if any.
+     *
+     * @param project the root project from which to begin collecting projects
+     * @return a set containing the root project and all its subprojects
+     */
+    public static Set<Project> getAllProjectsRecursive(Project project) {
+        Set<Project> projects = new HashSet<>();
+        collectProjectsRecursive(project, projects);
+        return projects;
+    }
+
+    /**
+     * Recursively collects all subprojects of the specified project and adds them to the provided set.
+     * This ensures that all hierarchical project relationships are traversed and included.
+     *
+     * @param project the project from which to start collecting subprojects
+     * @param projects the set to which collected projects will be added
+     */
+    private static void collectProjectsRecursive(Project project, Set<Project> projects) {
+        if (!projects.add(project))
+            return;
+        project.getSubprojects().forEach(p -> collectProjectsRecursive(p, projects));
     }
 }
